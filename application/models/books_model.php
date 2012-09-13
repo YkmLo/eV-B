bd8585c98bfd116ac3a687ee3f2479adbfad6914
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 16:27:39 +0700

    book_model update

--- application/models/books_model.php
+++ application/models/books_model.php
@@ -3,8 +3,13 @@
 class Books_model extends CI_Model{
 	private $table_name='books';
 	
-	public function _construct(){
+	public function __construct(){
 		$this->load->database();
 	}
-	
+	public function get_books(){
+		$query = $this->db->get('books');
+		$result= $query->result_array();
+		return $result;
+		//print_r($result);exit;
+	}
 }
