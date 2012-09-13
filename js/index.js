29c6fefa48591a3a90758b2f9efd5d0e81ca1992
esibero gats <esibero@gatsconsulting.com>
2012-09-13 12:58:22 +0700

    fb connect - index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,35 @@
 // JavaScript Document
 
-//register
-$('#signup_btn').click(function()
-	{
-       
+$('.fb_btn').click(function () {
+	fb_dialog = window.open('', 'Facebook', 'menubar=no,width=790,height=360,toolbar=no');
+  
+    var form_data = {
+        type : 'connect'
+    };
+    
+	$.ajax(
+    {
+        url: GD.base_url + "fb_connection",
+        type: 'POST',
+        async : true,
+        data : form_data,
+        dataType: 'json',
+        success: function(msg)
+        {
+            if (msg.status == "success")
+            {
+                fb_dialog.document.location.href = msg.url
+            }
+            else
+            {
+				fb_dialog.close();
+                alert(msg.reason);
+            	
+			}
+        }
+		,error: function(msg)
+		{
+			alert(msg.reason);
+		}
     });
-	
-	
-	
-// login	
-$('#login_btn').submit(function(){
-        
-    });
-	
+});
