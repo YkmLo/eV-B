457646a09c60af1402b643f113776e01b0bad089
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 13:27:23 +0700

    home page

---
+++ application/views/home.php
@@ -0,0 +1,12 @@
+<link href="<?php echo base_url(); ?>css/home.css" rel="stylesheet" type="text/css" />
+<div id="home_content">
+<div id="bookcase">
+    <?php for($j=0;$j<3;$j++):?>
+    <div class="book_collection">
+		<?php for($i=0;$i<3;$i++):?>
+            <img class="book" src="<?php echo base_url()?>images/book.png"/>
+        <?php endfor;?>
+    </div>
+    <?php endfor;?>
+</div>
+</div>
