88773af5b3e510dbace57fb54cfc6079f59694f8
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 14:15:11 +0700

    home button and css for button

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,8 @@
 <link href="<?php echo base_url(); ?>css/home.css" rel="stylesheet" type="text/css" />
-<div id="home_content">
+
+<div id="home_content"> 
+<input type="button"id="button_add" value="+ ADD"/>
+<div style="clear:both;"></div>
 <div id="bookcase">
     <?php for($j=0;$j<3;$j++):?>
     <div class="book_collection">
