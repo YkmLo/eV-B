867bd6a084569c5c356874244f6eb9e85aed91ea
Hendry Rusli <lie.hendry@gmail.com>
2012-09-13 14:43:59 +0700

    landing page logo

--- application/views/mobile/landing_page.php
+++ application/views/mobile/landing_page.php
@@ -1,5 +1,6 @@
 <link href="<?php echo base_url() ?>css/mobile/landing_page.css" type="text/css" rel="stylesheet" />
 
 <div data-role="content">
+   <img src="<?php echo base_url(); ?>/images/logo.png" id="logo" />   
    <a href="#"><div id="fb_button" onclick=""><div id="fb_text">Connect with facebook</div></div></a>
 </div>
