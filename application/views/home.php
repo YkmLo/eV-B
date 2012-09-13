816b3a32d6f93caeaac98d1d76fbcb80c4e8f987
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 15:18:46 +0700

    home add hastag,image

--- application/views/home.php
+++ application/views/home.php
@@ -1,13 +1,16 @@
 <link href="<?php echo base_url(); ?>css/home.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url();?>js/home.js" rel="stylesheet" type="text/javascript" />
 <div id="home_content"> 
-<a href="<?php base_url();?>/create"><input type="button"id="button_add" value="+ ADD"/></a>
+<a href="<?php echo base_url();?>/create"><input type="button"id="button_add" value="+ ADD"/></a>
 <div style="clear:both;"></div>
 <div id="bookcase">
     <?php for($j=0;$j<3;$j++):?>
     <div class="book_collection">
 		<?php for($i=0;$i<3;$i++):?>
-            <img class="book" src="<?php echo base_url()?>images/book.png"/>
+            <div class="book">
+            	<div ><img class="image_book" src="<?php echo base_url()?>/images/dummy.jpg" /></div>
+                <div class="image_hash">#hashtag</div>
+            </div>
         <?php endfor;?>
     </div>
     <?php endfor;?>
