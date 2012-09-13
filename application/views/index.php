a821e3bbea98c0450644232d3b2c1b73a80dcb0d
esibero gats <esibero@gatsconsulting.com>
2012-09-13 13:11:21 +0700

    add CI variable

--- application/views/index.php
+++ application/views/index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:fb="https://www.facebook.com/2008/fbml">
+<script type="text/javascript">
+	   var GD = {
+		  'base_url': '<?php echo base_url(); ?>'
+	   };
+</script>
    <head>
       <title>eV-B</title>
    </head>
-
+	
    <body>
 
       <div data-role="page">
