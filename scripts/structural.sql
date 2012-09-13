0f6a2c5465fc96e3e5d25e401deb994b0dbaf778
Djaka Widjaya Kurniawan <kurniawan_djaka@hotmail.com>
2012-09-13 12:09:35 +0700

    database structure changes

--- scripts/structural.sql
+++ scripts/structural.sql
@@ -9,11 +9,27 @@ CREATE TABLE `users` (
   PRIMARY KEY (`userid_pk`)
 ) ENGINE=InnoDB;
 
+CREATE TABLE `books` (
+  `bookid_pk` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` enum('private', 'public') NOT NULL,
+  `userid_fk` bigint(20) NOT NULL,
+  `date_created` datetime NOT NULL,
+  PRIMARY KEY (`bookid_pk`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `booksusers` (
+  `bookid_fk` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userid_fk` bigint(20) NULL,
+  `fb_id` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`bookid_fk`, `userid_fk`)
+) ENGINE=InnoDB;
+
 CREATE TABLE `items` (
   `itemid_pk` bigint(20) NOT NULL AUTO_INCREMENT,
   `type` enum('video', 'image') NOT NULL,
   `location` varchar(200) NOT NULL,
   `userid_fk` bigint(20) NOT NULL,
+  `bookid_fk` bigint(20) NOT NULL,
   `date_created` datetime NOT NULL,
   PRIMARY KEY (`itemid_pk`)
 ) ENGINE=InnoDB;
@@ -32,6 +48,18 @@ CREATE TABLE `hashesitems` (
 ALTER TABLE `items`
   ADD CONSTRAINT `items_users_useridfk` FOREIGN KEY (`userid_fk`) REFERENCES `users` (`userid_pk`) ON DELETE CASCADE;
 
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_books_bookidfk` FOREIGN KEY (`bookid_fk`) REFERENCES `books` (`bookid_pk`) ON DELETE CASCADE;
+  
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_users_useridfk` FOREIGN KEY (`userid_fk`) REFERENCES `users` (`userid_pk`) ON DELETE CASCADE;
+
+ALTER TABLE `booksusers`
+  ADD CONSTRAINT `booksusers_users_bookidfk` FOREIGN KEY (`bookid_fk`) REFERENCES `books` (`bookid_pk`) ON DELETE CASCADE;
+  
+ALTER TABLE `books`
+  ADD CONSTRAINT `booksusers_users_useridfk` FOREIGN KEY (`userid_fk`) REFERENCES `users` (`userid_pk`) ON DELETE CASCADE;
+  
 ALTER TABLE `hashesitems`
   ADD CONSTRAINT `hashesitems_items_itemidfk` FOREIGN KEY (`itemid_fk`) REFERENCES `items` (`itemid_pk`) ON DELETE CASCADE;
 
