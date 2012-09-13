b3fa7010543b56981bdb68a47f22020d459a299f
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 16:34:11 +0700

    home update

--- application/views/home.php
+++ application/views/home.php
@@ -3,16 +3,19 @@
 <div id="home_content"> 
 <a href="<?php echo base_url();?>/create"><input type="button"id="button_add" value="+ ADD"/></a>
 <div style="clear:both;"></div>
-<div id="bookcase">
-    <?php for($j=0;$j<3;$j++):?>
-    <div class="book_collection">
-		<?php for($i=0;$i<3;$i++):?>
-            <div class="book">
-            	<div ><img class="image_book" src="<?php echo base_url()?>/images/dummy.jpg" /></div>
-                <div class="image_hash">#hashtag</div>
-            </div>
+    <div id="bookcase">
+        <?php $flagdata=0; for($j=0;$j<$flag;$j++):?>
+        <div class="book_collection">
+            <?php  for($i=0;$i<3;$i++):?>
+               	<?php if($flagdata>=$total) break; ?>
+                <div <?php if($i==0):?> style="margin-left:90px;" <?php endif;?> class="book">
+                    <div ><img class="image_book" src="<?php echo base_url()?>/images/dummy.jpg" /></div>
+                    <div class="image_hash"><?php echo $data[$flagdata]['bookname']; ?></div>
+                </div>
+                
+     		<?php $flagdata++;  endfor; ?>
+            <div style="clear:both"></div>
+        </div>
         <?php endfor;?>
     </div>
-    <?php endfor;?>
-</div>
 </div>
