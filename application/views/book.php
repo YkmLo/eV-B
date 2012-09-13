d49b65cea6b729e60514a293167fbfeea8756514
Jeffry Antonio <bugslucky777@yahoo.com>
2012-09-13 14:41:18 +0700

    book enhancement

--- application/views/book.php
+++ application/views/book.php
@@ -3,18 +3,23 @@
 <script type="text/javascript" src="<?php echo base_url() ?>js/turn.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>js/book.js"></script>
 
-<div>
+<div id="content">
 
 <div id="book">
 	<div class="hard"><div class="side"></div></div>
-    <div class="hard fixed"></div>
+    <div class="hard">
+    	<div class="page-depth-left"></div>
+    </div>
     <div class="own-size"></div>
     <div class="own-size"></div>
     <div class="own-size"></div>
     <div class="own-size"></div>
     <div class="own-size"></div>
     <div class="own-size"></div>
-    <div class="hard fixed last-1"><div class="side"></div></div>
+    <div class="hard fixed beforelast">
+    	<div class="page-depth-right"></div>
+    	<div class="side"></div>
+    </div>
 	<div class="hard last"></div>
 </div>
 
--- js/book.js
+++ js/book.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 	$('#book').turn({
 		autoCenter : true	
-	});	
+	});
+	
+	$('#book').bind('turning', function(event, pageObject, corner){
+		if (pageObject == 4) {
+			$('.p2').addClass('fixed');
+		}
+		if (pageObject == 3) {
+			$('.p2').removeClass('fixed');
+		}
+		
+		if (pageObject == $('#book').turn('pages')-3) {
+			$('.beforelast').addClass('fixed');
+		}
+		
+		if (pageObject == $('#book').turn('pages')-2) {
+			$('.beforelast').removeClass('fixed');
+		}
+		
+		if(pageObject <= 40)		
+			$('.page-depth-left').width((pageObject-4)/2)
+		
+		if(pageObject >= $('#book').turn('pages')-40)		
+			$('.page-depth-right').width(($('#book').turn('pages')-pageObject-4)/2)
+		
+		
+	});
 });
