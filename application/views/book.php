045bae16e327c78767bc9b01931e18e686bf09ef
Jeffry Antonio <bugslucky777@yahoo.com>
2012-09-13 16:18:07 +0700

    book static

--- application/views/book.php
+++ application/views/book.php
@@ -1,5 +1,7 @@
+<link href="<?php echo base_url() ?>css/jquery.lightbox-0.5.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url() ?>css/book.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="<?php echo base_url() ?>js/jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url() ?>js/jquery.lightbox-0.5.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>js/turn.js"></script>
 <script type="text/javascript" src="<?php echo base_url() ?>js/book.js"></script>
 
@@ -10,9 +12,72 @@
     <div class="hard">
     	<div class="page-depth-left"></div>
     </div>
-    <div class="own-size"></div>
-    <div class="own-size"></div>
-    <div class="own-size"></div>
+    <div class="own-size">
+    	<div class="page_content" align="center">
+        	<a href="<?php echo base_url() ?>images/sample/asd.jpg">
+            	<div class="image_wrapper">
+            		<img class="image_item" src="<?php echo base_url() ?>images/sample/asd.jpg" />
+                </div>
+            </a>
+            <div class="image_description_wrapper">
+            	<div class="uploaded_by">Uploaded By</div>
+            	<div class="image_description_content">
+                    <div class="image_uploader">
+                        <img src="<?php echo base_url() ?>images/sample/123.jpg" />
+                    </div>
+                    <div class="image_description">
+                        <span class="image_uploader_name">Jeffry Antonio</span> some caption text
+                        
+                        <div class="timestamp">2 hours ago</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="own-size">
+    	<div class="page_content" align="center">
+        	<a href="<?php echo base_url() ?>images/sample/asd.jpg">
+            	<div class="image_wrapper">
+            		<img class="image_item" src="<?php echo base_url() ?>images/sample/asd.jpg" />
+                </div>
+            </a>
+            <div class="image_description_wrapper">
+            	<div class="uploaded_by">Uploaded By</div>
+            	<div class="image_description_content">
+                    <div class="image_uploader">
+                        <img src="<?php echo base_url() ?>images/sample/123.jpg" />
+                    </div>
+                    <div class="image_description">
+                        <span class="image_uploader_name">Jeffry Antonio</span> some caption text
+                        
+                        <div class="timestamp">2 hours ago</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="own-size">
+    	<div class="page_content" align="center">
+        	<a href="<?php echo base_url() ?>images/sample/asd.jpg">
+            	<div class="image_wrapper">
+            		<img class="image_item" src="<?php echo base_url() ?>images/sample/asd.jpg" />
+                </div>
+            </a>
+            <div class="image_description_wrapper">
+            	<div class="uploaded_by">Uploaded By</div>
+            	<div class="image_description_content">
+                    <div class="image_uploader">
+                        <img src="<?php echo base_url() ?>images/sample/123.jpg" />
+                    </div>
+                    <div class="image_description">
+                        <span class="image_uploader_name">Jeffry Antonio</span> some caption text
+                        
+                        <div class="timestamp">2 hours ago</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="own-size"></div>
     <div class="own-size"></div>
     <div class="own-size"></div>
