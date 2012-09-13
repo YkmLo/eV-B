d4e2f09d1474596cc729b3c1432c66b095367026
Freddy Wijaya <vald.karate@gmail.com>
2012-09-13 12:56:41 +0700

    change css

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,3 +1,3 @@
-<link href="<?php echo base_url() ?>css/home.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo base_url() ?>css/landing_page.css" type="text/css" rel="stylesheet" />
 <a href="#"><div id="fb_button"><div id="fb_text">Connect with facebook</div></div></a>
 
