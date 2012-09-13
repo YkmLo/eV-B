58a1a1676592b39c4842afaebab4129e2f56e6cd
Andreas Andy Jatmiko <andreazandy@hotmail.com>
2012-09-13 14:23:35 +0700

    flipix logo

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,4 +1,4 @@
 <link href="<?php echo base_url() ?>css/landing_page.css" type="text/css" rel="stylesheet" />
-
+<img src="<?php echo base_url(); ?>/images/logo.png" id="logo" />
 <a href="#"><div id="fb_button" onclick=""><div id="fb_text">Connect with facebook</div></div></a>
 
